3525ebdafc6506d0a33b8985fa6ebc5eea26287f
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2025-01-16 12:49:46 +0530

    changes in home view

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="main" class="d-flex justify-content-center align-items-center">
+    <h1>@mohitrajrathor</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style lang=""></style>
