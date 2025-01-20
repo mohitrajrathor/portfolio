985b89f85c3faf827d58be6308e7412b60251ac2
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2025-01-20 22:01:04 +0530

    new portfoilio

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,14 @@ import Home from "../components/Home.vue";
 import About from "../components/About.vue";
 import Projects from "../components/Projects.vue";
 import Contact from "../components/Contact.vue";
+import NotFound from "../components/NotFound.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", component: Home },
   { path: "/about", component: About },
   { path: "/projects", component: Projects },
   { path: "/contact", component: Contact },
+  { path: "/:pathMatch(.*)*", component: NotFound }, // Catch-all route
 ];
 
 const router = createRouter({
